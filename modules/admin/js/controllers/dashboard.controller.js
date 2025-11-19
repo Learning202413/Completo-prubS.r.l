8bbea41b81daa3912c496dbd4ac437845d189f29
Learning202413 <cjazztin7@gmail.com>
2025-11-19 08:43:49 -0500

    Delete modules directory

--- modules/admin/js/controllers/dashboard.controller.js
+++
@@ -1,9 +0,0 @@
-/**
- * js/controllers/dashboard.controller.js
- */
-export const DashboardController = {
-    init: function() {
-        console.log("DashboardController inicializado. Cargando datos de KPIs...");
-        // Lógica para cargar datos del dashboard (gráficos, KPIs)
-    }
-};
