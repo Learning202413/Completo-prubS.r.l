168b8068ee7ae4cdc5b299b91795e35e1406cebe
Learning202413 <cjazztin7@gmail.com>
2025-11-20 12:00:23 -0500

    Add files via upload

---
+++ inventario/js/controllers/reportes.controller.js
@@ -0,0 +1,10 @@
+/**
+ * js/controllers/reportes.controller.js
+ * Controlador para la vista de reportes de inventario.
+ */
+export const ReportesController = {
+    init: function() {
+        console.log("ReportesController (Inventario) inicializado.");
+        // Lógica para cargar gráficos y reportes
+    }
+};
